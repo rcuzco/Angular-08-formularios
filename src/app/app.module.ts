a90a8d68ebb0de56d6f9c5e9b2b1bd5a1cc4186f
Rony Cuzco <ronald.cuzco@gmail.com>
2018-11-18 10:22:25 +0100

    creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './components/template/template.component';
+import { DataComponent } from './components/data/data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
