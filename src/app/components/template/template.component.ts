5295e8b523db30e71754bdddceb42c29a0e23cf1
Rony Cuzco <ronald.cuzco@gmail.com>
2018-11-18 12:09:21 +0100

    avanzando cambios y ejercicios

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css']
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent implements OnInit
+{
+  usuario: Object =
+    {
+      nombre: null,
+      apellido: null,
+      correo: null,
+      pais: "",
+      sexo: "Hombre",
+      acepta: false
+    };
 
-  constructor() { }
-
-  ngOnInit() {
+  paises =
+    [
+      { codigo: "ESP", nombre: "España" },
+      { codigo: "PER", nombre: "Perú" },
+      { codigo: "BRA", nombre: "Brasil" },
+      { codigo: "FRA", nombre: "Francia" }
+    ];
+  constructor()
+  {
   }
 
+  ngOnInit()
+  {
+  }
+
+  guardar(forma:NgForm)
+  {
+    console.log("guardar!!");
+    console.info(forma);    
+  }
 }
